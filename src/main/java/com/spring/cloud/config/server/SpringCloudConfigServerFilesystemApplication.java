18dc79cabb91d09c5dc84a9c253f154f32312d3d
kotireddy <kotireddyare@gmail.com>
2018-06-23 17:54:29 +0530

    Initial Setup

--- src/main/java/com/spring/cloud/config/server/SpringCloudConfigServerFilesystemApplication.java
+++ src/main/java/com/spring/cloud/config/server/SpringCloudConfigServerFilesystemApplication.java
@@ -10,6 +10,5 @@ public class SpringCloudConfigServerFilesystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerFilesystemApplication.class, args);
-
 	}
 }
